27d84112485d0ac384270f4ea7404ce8ba31d6db
Kangyun Wang <wky@kangyuns-mbp.home>
2017-08-03 13:53:04 +0200

    Fix twitter link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let setContent = data => {
 	let tagless = data.content.replace(/(<[/]*\w+>)/g, '');
 	theQuote.innerHTML = data.content;
 	qAuthor.innerHTML = data.title;
-	twitterBtn.href = `https://twitter.com/intent/tweet?text=${encodeURI(tagless)}}`;
+	twitterBtn.href = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURI(tagless)}}`;
 }
 
 // bypass for bypassing cache
